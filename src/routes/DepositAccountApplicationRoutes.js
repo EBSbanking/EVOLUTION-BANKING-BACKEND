74f358db0acfdea09bdc36911a3ae71c37c1e85c
Frederick Agabi <Frederick.Agabi@fincanigeria.com>
2025-11-05 08:08:50 -0800

    first commit

--- src/routes/DepositAccountApplicationRoutes.js
+++ src/routes/DepositAccountApplicationRoutes.js
@@ -1,7 +1,7 @@
 // routes/depositAccountApplicationRoutes.js
 import express from "express";
 import DepositAccountApplicationController from "../controllers/DepositAccountApplicationController.js";
-import { getProductTypeByProdIdInternal } from "../services/productService.js";
+import { getProductTypeByProdIdInternal } from "../services/productservice.js";
 import { generateAccountIdentifiersFromCounter } from "../utils/accountHelper.js";
 
 const router = express.Router();
